b8285d422248d504be9197a79b3ec2a46059ec2e
unknown <dbsgywls667@gmail.com>
2026-04-14 12:50:11 +0900

    test: CD 확인용 health 클래스 추가\

--- candy/src/main/java/com/tjg_project/candy/domain/health/Health.java
+++ candy/src/main/java/com/tjg_project/candy/domain/health/Health.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+
 @RestController
 @RequestMapping("/qwe")
 public class Health {
 
     @GetMapping("/heal")
-    public String health() {
-        return "ok";
+    public Map<String, String> health() {
+        return  Map.of(
+                "status", "ok"
+        );
     }
 }
